32d79b48ed4f54b6a73300ae0d74de695aa026a5
alexandronesi@gmail.com <46le54si>
2022-01-08 23:17:40 +0100

    first commit

---
+++ src/main/java/com/a29340/sensorwebsocketdemo/SensorWebsocketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.a29340.sensorwebsocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorWebsocketDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SensorWebsocketDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/a29340/sensorwebsocketdemo/SensorWebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a29340.sensorwebsocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorWebsocketDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
